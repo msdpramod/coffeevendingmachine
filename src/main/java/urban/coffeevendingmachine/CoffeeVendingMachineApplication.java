52f3ae4044bc04ffab8fdfd4b9cb65fe952ad582
kuppapramod <kuppapramod@gmail.com>
2024-09-17 12:53:04 +0530

    hi

---
+++ src/main/java/urban/coffeevendingmachine/CoffeeVendingMachineApplication.java
@@ -0,0 +1,13 @@
+package urban.coffeevendingmachine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeVendingMachineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoffeeVendingMachineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/urban/coffeevendingmachine/CoffeeVendingMachineApplicationTests.java
@@ -0,0 +1,13 @@
+package urban.coffeevendingmachine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeVendingMachineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
